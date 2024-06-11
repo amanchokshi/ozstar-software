23b87685617f0209fa696d8edb6baba28bb8bd1e
Aman Chokshi <achokshi@student.unimelb.edu.au>
2024-06-11 14:17:06 +1000

    updating dependancies

--- modulefiles/hyperdrive/v0.3.0.AC.lua
+++ modulefiles/hyperdrive/v0.3.0.AC.lua
@@ -2,7 +2,7 @@ local tool = 'hyperdrive'
 local version = 'v0.3.0'
 help("Sets up the paths you need to use "..tool.." version "..version)
 whatis([[For further information see https://github.com/MWATelescope/mwa_hyperdrive]] )
-load("cuda/12.0.0", "cfitsio/3.49")
+load("cuda/12.0.0", "cfitsio/3.49.AC")
 
 if (mode() ~= "whatis") then
     local root = "/fred/oz048/achokshi/software/"..tool.."/"..version.."/"
