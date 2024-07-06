7133598e0356e4a98ce9db55fa8698ab144a7f48
Aman Chokshi <achokshi@student.unimelb.edu.au>
2024-07-06 22:33:33 +1000

    Added some beautiful colours to my log scripts

--- scripts/log.sh
+++ scripts/log.sh
@@ -2,5 +2,12 @@
 
 # Function to print a message with a timestamp including milliseconds
 log() {
-  echo "[ $(date '+%Y-%m-%d %H:%M:%S.%3N') ] $1"
+  # Define color codes
+  BLACK='\033[0;30m'
+  RED='\033[0;31m'
+  GREEN='\033[0;32m'
+  YELLOW='\033[0;33m'
+  BLUE='\033[0;34m'
+  NC='\033[0m' # No Color
+  echo -e "${BLACK}[${NC}$(date -u '+%Y-%m-%dT%H:%M:%SZ') ${BLUE} LOG ${BLACK}] ${NC}$1"
 }
