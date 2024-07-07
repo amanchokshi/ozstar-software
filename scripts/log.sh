d4e728f9581a6b7420ece8e3a33a33b66537f430
Aman Chokshi <achokshi@student.unimelb.edu.au>
2024-07-07 19:12:30 +1000

    colours for stdout, but not when redirecting to log file

--- scripts/log.sh
+++ scripts/log.sh
@@ -2,12 +2,18 @@
 
 # Function to print a message with a timestamp including milliseconds
 log() {
-  # Define color codes
-  BLACK='\033[0;30m'
-  RED='\033[0;31m'
-  GREEN='\033[0;32m'
-  YELLOW='\033[0;33m'
-  BLUE='\033[0;34m'
-  NC='\033[0m' # No Color
-  echo -e "${BLACK}[${NC}$(date -u '+%Y-%m-%dT%H:%M:%SZ') ${BLUE} LOG ${BLACK}] ${NC}$1"
+  # Check if stdout is a terminal
+  if [ -t 1 ]; then
+    # Define color codes
+    BLACK='\033[0;30m'
+    RED='\033[0;31m'
+    GREEN='\033[0;32m'
+    YELLOW='\033[0;33m'
+    BLUE='\033[0;34m'
+    NC='\033[0m' # No Color
+    echo -e "${BLACK}[${NC}$(date -u '+%Y-%m-%dT%H:%M:%SZ') ${BLUE} LOG ${BLACK}] ${NC}$1"
+  else
+    # No colors if not printing to a terminal
+    echo "[$(date -u '+%Y-%m-%dT%H:%M:%SZ')  LOG ] $1"
+  fi
 }
